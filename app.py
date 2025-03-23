3719838fe8edd12c59525f52a1bfec6ad34d273a
Emre Çelik <67646462+Orionza@users.noreply.github.com>
2025-03-23 12:22:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if audio_bytes:
             elif label == "negative":
                 bg_color = "#4b1c1c"  # koyu kırmızı
                 emoji = "😠"
-            elif label == "neutral"::  # neutral
+            elif label == "neutral":  # neutral
                 bg_color = "#1a2f4f"  # koyu mavi
                 emoji = "😐"
 
